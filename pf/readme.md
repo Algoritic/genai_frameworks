b9360050cfd52624acc5af4a8f33654f1ce911e6
Yeoh-Yi-Hang <yh_yeoh10@hotmail.com>
2025-05-20 15:21:19 +0800

    feat: e2e test for extraction endpoint

--- pf/readme.md
+++ pf/readme.md
@@ -1,1 +1,5 @@
 `pf flow test --flow tools.extract_json:extract_json --inputs ocr_output="Paris" schema="{\"name\":\"capital\",\"strict\":false,\"schema\":{\"type\":\"object\",\"properties\":{\"name\":{\"type\":\"string\",\"description\":\"Name of the country\"}}}}"`
+
+`uvicorn api.index:app --host 0.0.0.0 --port 8080 --reload --limit-max-requests 50000000`
+
+`pytest ./test/e2e.py`
--- test/e2e.py
+++ test/e2e.py
@@ -1,12 +1,49 @@
 import os
-import requests
-from fastapi import FastAPI, UploadFile, Form, BackgroundTasks
-from fastapi.responses import JSONResponse
+from pathlib import Path
 
-app = FastAPI()
+from fastapi.testclient import TestClient
+
+from api.index import app
+
+# app = FastAPI()
 
 
-@app.post("/callback")
-#echo the payload
-async def callback(data: dict):
-    return JSONResponse(content=data)
+# @app.post("/callback")
+# # echo the payload
+# async def callback(data: dict):
+#     return JSONResponse(content=data)
+
+
+client = TestClient(app)
+
+
+def test_extraction():
+    root_path = Path(os.path.dirname(os.path.abspath(__file__)))
+    test_payload_path = root_path / "test_payload.pdf"
+
+    with open(test_payload_path, "rb") as f:
+        test_schema = {
+            "name": "test",
+            "description": "test",
+            "schema": {
+                "type": "object",
+                "properties": {
+                    "name": {"type": "string"},
+                    "description": {"type": "string"},
+                },
+            },
+        }
+
+        # convert file to base64
+        import base64
+
+        base64_file = base64.b64encode(f.read()).decode("utf-8")
+        response = client.post(
+            "/extract",
+            data={
+                "files": [base64_file],
+                "json_schema": test_schema,
+                "callback_url": "http://localhost:8080/callback",
+            },
+        )
+        assert response.status_code == 202
