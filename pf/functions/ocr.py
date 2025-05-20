434ccb4916bf457c61bf45dd88219c4522f9ace5
Yeoh-Yi-Hang <yh_yeoh10@hotmail.com>
2025-05-20 15:19:30 +0800

    feat: configurable azure doc intelligence

--- pf/functions/ocr.py
+++ pf/functions/ocr.py
@@ -1,39 +1,44 @@
-import os
+import concurrent
 import time
 from typing import List
+
 from azure.ai.documentintelligence import DocumentIntelligenceClient
-from azure.ai.documentintelligence.models import DocumentAnalysisFeature, DocumentContentFormat
+from azure.ai.documentintelligence.models import (
+    DocumentAnalysisFeature,
+    DocumentContentFormat,
+)
 from azure.core.credentials import AzureKeyCredential
-import concurrent
-from settings import app_settings
 from logger import logger
+from promptflow.tracing import trace
+from settings import app_settings
 
 
-def use_azure_document_intelligence(bytes: bytes) -> str:
+@trace
+def use_azure_document_intelligence(
+    bytes: bytes,
+    model_name="prebuilt-layout",
+    features=[DocumentAnalysisFeature.OCR_HIGH_RESOLUTION],
+    api_version="2024-11-30",
+) -> str:
     client = DocumentIntelligenceClient(
+        api_version=api_version,
         endpoint=app_settings.azure_document_intelligence.endpoint,
-        credential=AzureKeyCredential(
-            app_settings.azure_document_intelligence.api_key))
+        credential=AzureKeyCredential(app_settings.azure_document_intelligence.api_key),
+    )
     poller = client.begin_analyze_document(
-        "prebuilt-layout",
-        # "prebuilt-check.us",
+        model_id=model_name,
         body=bytes,
-        features=[
-            # DocumentAnalysisFeature.KEY_VALUE_PAIRS,
-            DocumentAnalysisFeature.OCR_HIGH_RESOLUTION
-        ],
+        features=features,
         output_content_format=DocumentContentFormat.TEXT,
     )
     result = poller.result()
-    #calculate average, min, max confidence score
+    # calculate average, min, max confidence score
     average_confidence = 0
     min_confidence = 0
     max_confidence = 0
     word_count_list = [len(page.words) for page in result.pages]
     word_count = sum(word_count_list)
-    confidences = [
-        word.confidence for page in result.pages for word in page.words
-    ]
+    confidences = [word.confidence for page in result.pages for word in page.words]
     if len(confidences) > 0:
         average_confidence = round(sum(confidences) / len(confidences), 2)
         min_confidence = min(confidences)
@@ -43,7 +48,7 @@ def use_azure_document_intelligence(bytes: bytes) -> str:
         "average_confidence": average_confidence,
         "min_confidence": min_confidence,
         "max_confidence": max_confidence,
-        "word_count": word_count
+        "word_count": word_count,
     }
 
     return {
@@ -52,8 +57,8 @@ def use_azure_document_intelligence(bytes: bytes) -> str:
     }
 
 
+@trace
 def parallel_document_intelligence(image_bytes: List[bytes]):
-
     batch_size = 15
     total_pages = len(image_bytes)
     results = [None] * total_pages
@@ -71,8 +76,7 @@ def parallel_document_intelligence(image_bytes: List[bytes]):
 
         batch_start_time = time.time()
         # Process the current batch in parallel
-        with concurrent.futures.ThreadPoolExecutor(
-                max_workers=batch_size) as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=batch_size) as executor:
             # Submit all jobs with their original indices
             future_to_idx = {
                 executor.submit(process_with_index, (idx, image_byte)): idx
@@ -82,10 +86,9 @@ def parallel_document_intelligence(image_bytes: List[bytes]):
             # Collect results as they complete
             for future in concurrent.futures.as_completed(future_to_idx):
                 idx, ocr_result = future.result()
-                results[idx] = ocr_result[
-                    'text']  # Store result at original index
-                if 'metrics' in ocr_result and ocr_result['metrics']:
-                    metrics[idx] = ocr_result['metrics']
+                results[idx] = ocr_result["text"]  # Store result at original index
+                if "metrics" in ocr_result and ocr_result["metrics"]:
+                    metrics[idx] = ocr_result["metrics"]
 
         # Calculate time to wait for rate limiting
         batch_processing_time = time.time() - batch_start_time
